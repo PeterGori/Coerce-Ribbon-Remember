f4d1fb72f4e3a69506a083ecbb1eac380c4f913f
PeterGori <Peter.W.Gori@gmail.com>
2024-12-16 15:01:56 +0000

    Doors and Buttons

--- Assets/Scripts/Objects/Button.cs
+++ Assets/Scripts/Objects/Button.cs
@@ -4,18 +4,49 @@ public class Button : MonoBehaviour
 {
     public Rigidbody2D ButtonRb;
     public Animator ButtonAnim;
+    public Transform PressCheck;
+    public LayerMask PressLayers;
+    public BoxCollider2D ButtonCollider;
+    public Vector2 PressCheckRadius;
+
     public bool IsPressed = false;
+    public bool Active = false;
+    private float time = 0;
     
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
         ButtonRb = GetComponent<Rigidbody2D>();
         ButtonAnim = GetComponent<Animator>();
+        ButtonCollider = GetComponent<BoxCollider2D>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if (IsPressed)
+        {
+            ButtonAnim.SetBool("Pressed", true);
+            ButtonCollider.size = new Vector2(1f,0.25f);
+            time = 0.25f;
+            Active = true;
+        }
+        else
+        {
+            time -= Time.deltaTime;
+            if (time < 0f)
+            {
+                ButtonAnim.SetBool("Pressed", false);
+                ButtonCollider.size = new Vector2(1f,1f);
+                Active = false;
+            }
+        }
+        CheckSurroundings();
     }
+    
+    private void CheckSurroundings()
+    {
+        IsPressed = Physics2D.OverlapBox(PressCheck.position, PressCheckRadius, 0, PressLayers);
+    }
+    
 }
--- Assets/Scripts/Objects/Door.cs
+++ Assets/Scripts/Objects/Door.cs
@@ -2,15 +2,48 @@ using UnityEngine;
 
 public class Door : MonoBehaviour
 {
+    public BoxCollider2D DoorCollider;
+    public Animator DoorAnim;
+    public GameObject Button;
+    public bool IsOpen = false;
+    private float StartY;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        
+        DoorCollider = GetComponent<BoxCollider2D>();
+        DoorAnim = GetComponent<Animator>();
+        Button = GameObject.Find("Button");
+        StartY = DoorCollider.transform.position.y;
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if (IsOpen)
+        {
+            DoorAnim.SetBool("Open", true);
+            DoorCollider.enabled = false;
+            // DoorCollider.size = new Vector2(1f,0f);
+            // DoorCollider.transform.position = new Vector2(DoorCollider.transform.position.x, StartY + 2f);
+        }
+        else
+        {
+            DoorAnim.SetBool("Open", false);
+            DoorCollider.enabled = true;
+            // DoorCollider.size = new Vector2(1f,1f);
+            // DoorCollider.transform.position = new Vector2(DoorCollider.transform.position.x, StartY);
+        }
+        CheckButton();
+    }
+    
+    private void CheckButton()
+    {
+        IsOpen = Button.GetComponent<Button>().Active;
+    }
+
+    private void OnDrawGizmos()
+    {
+        Gizmos.color = Color.red;
+        Gizmos.DrawWireCube(transform.position, DoorCollider.size);
     }
 }
